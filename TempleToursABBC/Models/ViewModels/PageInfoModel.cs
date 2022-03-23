808a1fdfc6275fbd50a7ee9e34169eae1e222252
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-03-23 11:45:43 -0600

    set up pagination

---
+++ TempleToursABBC/Models/ViewModels/PageInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleToursABBC.Models.ViewModels
+{
+    public class PageInfoModel
+    {
+        public int TotalNumTimes { get; set; }
+        public int TimesPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)Math.Ceiling(((double)TotalNumTimes / TimesPerPage));
+    }
+}
