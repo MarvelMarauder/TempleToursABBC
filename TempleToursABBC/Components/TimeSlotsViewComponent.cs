733343fb6d82c50a4ab8b7b7d1b3f9a10e3de467
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-03-23 09:19:15 -0600

    Attempt to start a view component

---
+++ TempleToursABBC/Components/TimeSlotsViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TempleToursABBC.Models;
+
+namespace TempleToursABBC.Components
+{
+    public class TimeSlotsViewComponent : ViewComponent
+    {
+        private AppointmentContext context { get; set; }
+
+        public TimeSlotsViewComponent(AppointmentContext temp)
+        {
+            context = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var timeSlots = context.TimeSlots
+                .Select(x => x.Slot)
+                .Distinct()
+                .OrderBy(x => x.Date);
+
+
+            return View(timeSlots);
+        }
+    }
+}
